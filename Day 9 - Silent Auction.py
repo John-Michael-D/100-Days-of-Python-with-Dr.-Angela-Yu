ef0986a05bd3a932c30eabdd1a8229629e0b2d5a
John-Michael-D <johnm.dantes@yahoo.com>
2025-05-03 21:55:12 -0400

    Add files via upload

---
+++ Day 9 - Silent Auction.py
@@ -0,0 +1,73 @@
+import os, sys
+
+os.system("clear")
+
+auctionDict = {
+
+}
+
+
+def auctionWinner():
+    winner = ""
+    max = 0
+    i = 0
+    for key, value in auctionDict.items():
+        i += 1
+        if value > max:
+            max = value
+            winner = key
+        if i == len(auctionDict):
+            print(r"""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⢯⠙⠩⠀⡇⠊⠽⢖⠆⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠱⣠⠀⢁⣄⠔⠁⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⣷⣶⣾⣾⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⢀⡔⠙⠈⢱⡟⣧⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⡠⠊⠀⠀⣀⡀⠀⠘⠕⢄⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⢀⠞⠀⠀⢀⣠⣿⣧⣀⠀⠀⢄⠱⡀⠀⠀⠀
+                    ⠀⠀⡰⠃⠀⠀⢠⣿⠿⣿⡟⢿⣷⡄⠀⠑⢜⢆⠀⠀
+                    ⠀⢰⠁⠀⠀⠀⠸⣿⣦⣿⡇⠀⠛⠋⠀⠨⡐⢍⢆⠀
+                    ⠀⡇⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣦⡀⠀⢀⠨⡒⠙⡄
+                    ⢠⠁⡀⠀⠀⠀⣤⡀⠀⣿⡇⢈⣿⡷⠀⠠⢕⠢⠁⡇
+                    ⠸⠀⡕⠀⠀⠀⢻⣿⣶⣿⣷⣾⡿⠁⠀⠨⣐⠨⢀⠃
+                    ⠀⠣⣩⠘⠀⠀⠀⠈⠙⣿⡏⠁⠀⢀⠠⢁⡂⢉⠎⠀
+                    ⠀⠀⠈⠓⠬⢀⣀⠀⠀⠈⠀⠀⠀⢐⣬⠴⠒⠁⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀
+""")
+            print(f"The winner of the auction is {winner}. They bid ${max:.2f}!")
+            sys.exit()
+
+
+def logo():
+    print(r'''
+                             ___________
+                             \         /
+                              )_______(
+                              |"""""""|_.-._,.---------.,_.-._
+                              |       | | |               | | ''-.
+                              |       |_| |_             _| |_..-'
+                              |_______| '-' `'---------'` '-'
+                              )"""""""(
+                             /_________\\
+                           .-------------.
+                          /_______________\\
+    ''')
+
+
+while True:
+    logo()
+    userInputName = input("Enter your name:\n")
+    userInputBid = float(input("Enter your bid:\n$"))
+    auctionDict[userInputName] = userInputBid
+
+    while True:
+        try:
+            restartQuestion = input("\nAre there other bidders? Enter y for yes or n for no:\n").strip().lower()
+            if restartQuestion == "y":
+                os.system("clear")
+                break
+            elif restartQuestion == "n":
+                auctionWinner()
+            else:
+                print("Invalid entry! Enter y or n only!")
+        except ValueError:
+            print("Try again!")
